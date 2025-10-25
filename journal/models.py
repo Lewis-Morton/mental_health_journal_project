5b8e8f4d365fdddd4807e185a2addbc65385cdb6
Lewis <lewiswilliammorton@outlook.com>
2025-10-25 18:17:45 +0100

    make migration of initial db schema

--- journal/models.py
+++ journal/models.py
@@ -2,28 +2,36 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.IntegerField(max_length=20)
+    id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=30)
-    email = models.CharField(maxlength=80)
+    email = models.CharField(max_length=80)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     password = models.CharField(max_length=200)
 
 class JournalEntry(models.Model):
-    mood_choices = [('Content', 'Melancholy', 'Enthusiastic', 'Sorrowful')]
+    MOOD_CHOICES = [('content', 'Content'),
+                     ('melancholy', 'Melancholy'), 
+                     ('sorrowful', 'Sorrowful'),
+                     ('grateful', 'Grateful'),
+                     ('energetic', 'Energetic'),
+                     ('positive', 'Positive'),
+                     ('motivated', 'Motivated'),
+                     ('tired', 'Tired'),
+                     ('depressive', 'Depressive')]
 
-    id = models.IntegerField(max_length=20)
+    id = models.IntegerField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField()
     text = models.TextField(max_length=5000)
-    mood = models.CharField(max_length=20, choices=mood_choices)
+    mood = models.CharField(max_length=20, choices=MOOD_CHOICES)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
 class ChatMessage(models.Model):
-    id = models.IntegerField(max_length=20)
-    sender = models.ForeignKey(JournalEntry, on_delete=models.CASCADE)
-    recipient = models.ForeignKey(JournalEntry, on_delete=models.CASCADE )
+    id = models.IntegerField(primary_key=True)
+    sender = models.ForeignKey(JournalEntry, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(JournalEntry, on_delete=models.CASCADE, related_name='received_messages')
     text = models.CharField(max_length=2000)
     timestamp = models.TimeField()
-    room_id = models.IntegerField(max_length=20)
+    room_id = models.IntegerField()
