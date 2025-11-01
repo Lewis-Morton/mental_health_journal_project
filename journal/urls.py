9c4191f04826f0d1ce4c11d212530bf7226318e2
Lewis <lewiswilliammorton@outlook.com>
2025-11-01 13:22:59 +0000

    Update views and urls to include views dealing with CRUD for journalentry model

--- journal/urls.py
+++ journal/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from.views import JournalDetailAPIView
+from.views import JournalUpdateAPIView 
+from .views import JournalCreateAPIView
+from .views import JournalDestroyAPIView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('journals/<int:pk>/', JournalDetailAPIView.as_view(), name='journal-detail-api')
+    path('journals/<int:pk>/', JournalDetailAPIView.as_view(), name='journal-detail-api'),
+    path('journals/<int:pk>/', JournalUpdateAPIView.as_view(), name='journal-update-api'),
+    path('journals/<int:pk>/', JournalCreateAPIView.as_view(), name='journal-update-api'),
+    path('journals/<int:pk>/', JournalDestroyAPIView.as_view(), name='journal-destroy-api')
 ]
 
--- journal/views.py
+++ journal/views.py
@@ -4,8 +4,22 @@ from.models import JournalEntry
 from.serializers import JournalEntrySerializer
 
 # Create your views here.
+# Views for journalentry model
+# These views handle CRUD operations on journal entries
+class JournalCreateAPIView(generics.CreateAPIView):
+    queryset = JournalEntry.objects(all)
+    serializer_class = JournalEntrySerializer
 
 class JournalDetailAPIView(generics.RetrieveAPIView):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
+
+class JournalUpdateAPIView(generics.UpdateAPIView):
+    queryset = JournalEntry.objects.all()
+    serializer_class = JournalEntrySerializer
+
+class JournalDestroyAPIView(generics.DestroyAPIView):
+    queryset = JournalEntry.objects.all()
+    serializer_class = JournalEntrySerializer
+
 
